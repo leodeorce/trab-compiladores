d1f1d28f59e5b8c5d9e95907637eb445971f9791
leodeorce <42984982+leodeorce@users.noreply.github.com>
2021-02-27 18:24:40 -0300

    Palavras, comentários e pontuadores
    - Divisão visual dos tipos de palavras reconhecidas;
    - Tratamento de comentários;
    - Adição dos "pontuadores" (tradução ao pé da letra);
    - Erro se o identificador inicia com número (ainda falta reconhecer outros caracteres no ID);
    - Entrada de teste com mais alguns elementos.

---
+++ entrada.ts
@@ -0,0 +1,14 @@
+/* 
+ * Comentario 1
+ */
+
+// Comentario 2
+
+let awaiting = 2;
+let b2 = 3;
+// let 3c = "teste"; // Invalid ID
+let c3c = "teste";
+
+function soma(a: number, b: number): number {
+  return a + b;
+}
