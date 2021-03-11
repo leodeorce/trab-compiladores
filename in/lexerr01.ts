2b971176193f446e615be9d495d7be6c23ac199f
leodeorce <42984982+leodeorce@users.noreply.github.com>
2021-03-11 17:20:35 -0300

    Modificações nos casos com erros léxicos

---
+++ in/lexerr01.ts
@@ -0,0 +1,7 @@
+//Sample program in TypeScript language
+//Program with Lexical Error
+
+let @x:number;
+@x = 50;
+
+console.log(@x);
---
+++ in/lexerr03.ts
@@ -0,0 +1,7 @@
+//Sample program in TypeScript Language
+//Program with lexical errors
+
+let x:number;
+x = 6 # 7;
+
+console.log(x);
