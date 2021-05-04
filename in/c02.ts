a3612b6d80cf99f8812a32e0a7c78f81f201cb19
leodeorce <42984982+leodeorce@users.noreply.github.com>
2021-05-03 22:30:41 -0300

    Delete in directory

--- in/c02.ts
+++
@@ -1,7 +0,0 @@
-// Soma duas variáveis e atribui o valor à outra.
-
-let x_var = 3;
-let y_var:number = 2;
-let c_var:number; 
-c_var = x_var + yvar;
-console.log('se pa da certo', c);
--- in/c08.ts
+++
@@ -1,6 +0,0 @@
-// Mistura de tipos como argumento de função.
-
-console.log('Test');
-console.log(3 + 2);
-console.log(2 + "2");
-console.log(2 / 23);
